8b94da090309865406c3f321fb526db0e8ee0e3c
natagetup <n.kulikouskaya@gmail.com>
2024-01-16 21:13:29 +0100

    feat: add 02-write-file task solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+//Import all required modules.
+const path = require('path');
+const fs = require('fs');
+const { stdin } = process;
+
+const filePath = path.join(__dirname, 'text.txt');
+const writeStream = fs.createWriteStream(filePath);
+
+console.log('Hi! Enter text (Ctrl+C or type "exit" to exit):');
+
+stdin.on('data', (input) => {
+  const userInput = input.toString();
+  if (userInput.toLowerCase() === 'exit') {
+    console.log('Bye!...');
+    process.exit(0);
+  } else {
+    writeStream.write(`${userInput}`, (err) => {
+      if (err) {
+        console.error('Error:', err);
+        process.exit(1);
+      }
+      console.log('You can enter more text (Ctrl+C or type "exit" to exit):');
+    });
+  }
+});
+
+process.on('SIGINT', () => {
+  console.log('\nBye!...');
+  process.exit(0);
+});
