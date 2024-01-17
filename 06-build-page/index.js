17c6b8d3cfd26e7148062d453115453afb0b3561
natagetup <n.kulikouskaya@gmail.com>
2024-01-17 21:53:16 +0100

    feat: add style bundling && assets move to 06-build-page task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,10 +5,14 @@ const bundleFolderPath = path.join(__dirname, 'project-dist/');
 const bundleHtmlPath = path.join(__dirname, 'project-dist/index.html');
 const templatePath = path.join(__dirname, 'template.html');
 const componentsFolderPath = path.join(__dirname, 'components/');
+const assetsFolderPath = path.join(__dirname, 'assets/');
+const copyFolderPath = path.join(__dirname, 'project-dist/assets/');
 
-fs.promises.mkdir(bundleFolderPath, { recursive: true }, (err) => {
-  if (err) throw err;
-});
+fs.promises
+  .mkdir(bundleFolderPath, { recursive: true }, (err) => {
+    if (err) throw err;
+  })
+  .then(() => copyDir(assetsFolderPath, copyFolderPath));
 
 const readHtml = (path) => {
   return new Promise((resolve, reject) => {
@@ -52,7 +56,74 @@ readHtml(templatePath).then((template) => {
             );
             return result;
           })
-          .then((template) => writeHtml(bundleHtmlPath, template));
+          .then((template) => {
+            writeHtml(bundleHtmlPath, template);
+          });
       }
     });
 });
+
+//styles bundling
+const folderPath = path.join(__dirname, 'styles/');
+const bundleFilePath = path.join(__dirname, 'project-dist/style.css');
+const readData = [];
+
+fs.promises.readdir(folderPath, { withFileTypes: true }).then((filenames) => {
+  for (let filename of filenames) {
+    const filePath = filename.path + filename.name;
+    fs.stat(filePath, (err, stats) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      if (stats.isFile() && path.extname(filePath) === '.css') {
+        readSrcFiles(filePath);
+      }
+    });
+  }
+});
+
+const readSrcFiles = (filePath) => {
+  const readStream = fs.createReadStream(filePath, 'utf8');
+  readStream.on('data', (data) => {
+    readData.push(data);
+    fillBundle();
+  });
+  readStream.on('error', (err) => {
+    console.error(`Error: ${err.message}`);
+  });
+};
+
+const fillBundle = () => {
+  const writeStream = fs.createWriteStream(bundleFilePath);
+  writeStream.write(`${readData.join('')}`, (err) => {
+    if (err) {
+      console.error('Error:', err);
+    }
+  });
+};
+
+//assets copying into dist folder
+
+async function copyDir(src, dest) {
+  try {
+    await fs.promises.mkdir(dest, { recursive: true });
+
+    const srcFiles = await fs.promises.readdir(src);
+
+    for (const file of srcFiles) {
+      const srcPath = path.join(src, file);
+      const destPath = path.join(dest, file);
+
+      const fileStats = await fs.promises.stat(srcPath);
+
+      if (fileStats.isDirectory()) {
+        await copyDir(srcPath, destPath);
+      } else {
+        await fs.promises.copyFile(srcPath, destPath);
+      }
+    }
+  } catch (err) {
+    console.error('Error:', err);
+  }
+}
