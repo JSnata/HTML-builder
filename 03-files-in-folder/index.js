58fbae47491de1e2e8a7d0c8a500494989739398
natagetup <n.kulikouskaya@gmail.com>
2024-01-17 14:25:08 +0100

    feat: add 03-files-in-folder task solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'secret-folder/');
+
+fs.promises
+  .readdir(folderPath, { withFileTypes: true })
+  .then((filenames) => {
+    for (let filename of filenames) {
+      const filePath = filename.path + filename.name;
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        if (stats.isFile()) {
+          const name = filename.name.substr(0, filename.name.lastIndexOf('.'));
+          const extension = path.extname(filePath).slice(1);
+          const size = stats.size;
+          console.log(`${name} - ${extension} - ${size}`);
+        }
+      });
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
